b18e0e2d42ddec862e7e4db49f475063be726e3f
josephgreffenkomala <josephgreffenkomala@mail.ugm.ac.id>
2026-07-14 13:51:37 +0700

    a

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -97,7 +97,7 @@ class PreviewSession implements vscode.Disposable {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${this.panel.webview.cspSource} data: https:; style-src ${this.panel.webview.cspSource}; script-src 'nonce-${nonce}';">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${this.panel.webview.cspSource} data: https:; style-src ${this.panel.webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';">
     <link rel="stylesheet" href="${styleUri}">
     <title>render-mdx Preview</title>
   </head>
